7ad5fb89e27352115ccb57c65cb33174164ef0c2
Aneesh <136454406+lhcee3@users.noreply.github.com>
2024-04-07 23:07:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import ollama
+import streamlit as st 
+
+st.title("Senpai Chat Bot")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+
+if "model" not in st.session_state:
+    st.session_state["model"] = ""
+
+models = [model["name"] for model in ollama.list()["models"]]
+st.session_state["model"] = st.selectbox("Choose your model", models)
+
+def model_res_generator():
+    stream = ollama.chat(
+        model=st.session_state["model"],
+        messages=st.session_state["messages"],
+        stream=True,
+    )
+    for chunk in stream:
+        yield chunk["message"]["content"]
+
+# Display chat messages from history 
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state["messages"].append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message = st.write_stream(model_res_generator())
+        st.session_state["messages"].append({"role": "assistant", "content": message})
